08763a9d0a0e79c952d627c702d8f9b4edeb14c3
Ethan Wong <ethanwong98@yahoo.ca>
2023-04-14 13:47:22 -0700

    Set up Personal website

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+function Header() {
+	return (
+		<div class='headerPage'>
+			<div className='nav'></div>
+			<h1>ETHAN</h1>
+			<h1>NICHOLAS WONG</h1>
+			<h3>Full-Stack Software Developer</h3>
+		</div>
+	);
+}
+
+export default Header;
