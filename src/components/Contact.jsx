02fd97ec15f72713046ea927209782526e3d5c4a
Ethan Wong <118866423+ethanw03@users.noreply.github.com>
2023-04-16 01:48:58 -0700

    add contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,11 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Contact.sass';
 
 function Contact() {
+	const [formData, setFormData] = useState({
+		name: '',
+		email: '',
+		message: '',
+	});
+
+	const handleChange = (e) => {
+		setFormData({ ...formData, [e.target.name]: e.target.value });
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		console.log(formData);
+		// Send formData to your backend or API
+	};
+
 	return (
 		<div className='contactPage'>
 			<div className='contactContent'>
 				<h1>CONTACT</h1>
+			</div>
+			<div className='container'>
+				<div className='contact-info'>
+					<p>Ethan N. Wong</p>
+					<p>ethanwong98@yahoo.ca</p>
+					<p>LinkedIn</p>
+					<p>Creddle</p>
+					<p>Github</p>
+				</div>
+				<div className='divider'></div>
+				<form className='contact-form' onSubmit={handleSubmit}>
+					<input
+						type='text'
+						name='name'
+						placeholder='Name'
+						value={formData.name}
+						onChange={handleChange}
+						required
+					/>
+					<input
+						type='email'
+						name='email'
+						placeholder='Email'
+						value={formData.email}
+						onChange={handleChange}
+						required
+					/>
+					<textarea
+						name='message'
+						placeholder='Message'
+						value={formData.message}
+						onChange={handleChange}
+						required></textarea>
+					<button type='submit'>Submit</button>
+				</form>
 			</div>
 		</div>
 	);
---
+++ src/components/ImageSlider.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ImageSlider = () => {
+	return (
+		<div className='image-slider'>
+			<h1>Image Slider</h1>
+		</div>
+	);
+};
+
+export default ImageSlider;
